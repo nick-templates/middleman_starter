76e0f32a73776a2fd8a204d37f34c30955d831bf
Your Name <you@example.com>
2016-09-22 08:43:06 -0700

    stuff

--- config.rb
+++ config.rb
@@ -20,13 +20,13 @@ page '/*.txt', layout: false
 # Helpers
 ###
 
-activate :directory_indexes
+
 
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "/riot_test"
 
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  blog.permalink = "articles/{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
   blog.sources = "articles/{year}-{month}-{day}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
@@ -46,6 +46,8 @@ activate :blog do |blog|
   # blog.per_page = 10
   # blog.page_link = "page/{num}"
 end
+
+activate :directory_indexes
 
 page "/feed.xml", layout: false
 # page "/articles/*", layout: "single_page_layout"
@@ -67,6 +69,7 @@ configure :build do
   # Minify CSS on build
   # activate :minify_css
   set :build_dir, 'docs'
+  # blog.sources = "/riot_test/articles/{year}-{month}-{day}-{title}.html"
   # Minify Javascript on build
   # activate :minify_javascript
   config[:host_prefix] = "/riot_test"
